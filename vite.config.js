d092ce057d56962cb5a627563e0d3fa33d86b1bc
Asaduj Zaman <asadujzaman.cst@gmail.com>
2025-08-13 12:52:15 +0600

    Fix Vite config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000', // Django সার্ভার এড্রেস
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '') // অপশনাল: URL রি-রাইট
-      },
-    },
-  },
   build: {
-    outDir: '../backend/frontend/dist', // Django-র স্ট্যাটিক ফাইল লোকেশন
-    emptyOutDir: true,
+    outDir: 'dist',       // ✅ Vercel expects this folder
+    emptyOutDir: true
   }
 });
